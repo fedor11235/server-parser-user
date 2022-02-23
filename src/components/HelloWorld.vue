a3911a0fac4ffbbbe7ac52bdcade9124d54ce908
Fedor <f.avdeev@list.ru>
2022-02-23 23:05:09 +0300

    Table made

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h1>{{ prices }}</h1>
-    <h1>{{ books }}</h1>
+
+    <table border="1" align="center" class="prices">
+
+      <td>
+        <tr>
+          название книги/магазина
+        </tr>
+        <tr v-for="(book, i) in endjson[0].books" :key="i">
+          <th >{{book.info.title}}</th>
+        </tr>
+      </td>
+
+      <td v-for="(shopTitle, index) in endjson" :key="index" >
+        <th>
+          {{shopTitle.shop}}
+        </th>
+        <tr v-for="(book, i) in shopTitle.books" :key="i">
+          <td >{{book.price}}</td>
+        </tr>
+      </td>
+    </table>
+
+    <button v-for="(shopTitle, index) in endjson" :key="index" >{{ shopTitle.shop }}</button>
+
+      <br><br>
+
+    <select v-model="active">
+      <option v-for="(shopTitle, index) in endjson" :key="index" >{{ shopTitle.shop }}</option>
+    </select>
+
+      <br><br>
+
+    <label v-for="(shopTitle, index) in endjson" :key="index" >
+      <input type="radio" name="type" :value="shopTitle.shop" v-model="active">
+      {{ shopTitle.shop }}
+    </label>
 
   </div>
 </template>
@@ -11,27 +44,65 @@
 import api from '@/api'
 export default {
   name: 'HelloWorld',
-  prices: api.prices,
-  books: api.books,
   props: {
     msg: String
+  },
+  data() {
+    return {
+      prices: api.prices,
+      authors: api.books.authors,
+    }
+  },
+
+  computed: {
+    endjson: function (){
+      const resultArr=[]
+      for(const elem of this.prices) {
+        let existElem = false
+        resultArr.forEach(e=>{
+          if(e.shop===elem.shop) {
+            existElem = true
+            e.books.push({
+              'info':this.bookTitle(this.authors, elem.book_id), 
+              'price':elem.price})
+          }
+        })
+        if(!existElem) resultArr.push({
+          'shop': elem.shop, 
+          'books':[{
+            'info':this.bookTitle(this.authors, elem.book_id), 
+            'price':elem.price,
+            }
+          ]}) 
+      }
+      return resultArr
+    }
+  },
+
+  methods: {
+
+    bookTitle(arr2, book_id){
+      let result
+      for(const elem of arr2){
+
+        elem.books.forEach(e=>{
+          if(e.id===book_id) result = {'title': e.title, 'genre': e.genre}
+        })
+      }
+      return result
+    }
   }
 }
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.prices {
+  margin-bottom: 10px;
+  margin-top: 10px;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.books {
+
 }
 </style>
